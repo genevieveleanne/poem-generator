3b2355c6c058c6c366b396d3f48b611589baf0f8
Genevieve Leanne Dominguez <genevieveleanned25@gmail.com>
2025-10-15 11:03:02 -0400

    Created initial generate function for poem generator form
    Displays poem example text when form is submitted

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("Hello world!");
+function displayPoem(event) {
+  event.preventDefault();
+
+  let poem = document.querySelector("#poem");
+  poem.innerHTML = "The poem will go here";
+}
+
+let poemGeneratorForm = document.querySelector("#poem-generator-form");
+poemGeneratorForm.addEventListener("submit", displayPoem);
