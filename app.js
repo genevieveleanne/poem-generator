fe242bd969089983b3006da21f1307c6397f63c7
Genevieve Leanne Dominguez <genevieveleanned25@gmail.com>
2025-10-16 14:06:22 -0400

    Added axios and integrated AI API to display a poem about the user's input

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-function displayPoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "The poem will go here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 75,
+    cursor: "",
   });
 }
 
+function getAiApi() {
+  let userInput = document.querySelector("#user-input");
+
+  let apiKey = "1bac80fa0c32ft537387a483f19bf3fo";
+  let prompt = `Please write a 4 line poem in English about ${userInput.value}`;
+  let context =
+    "You are a poet who loves to write poems that rhyme. Please separate the first three lines with a <br />.";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  getAiApi();
+}
+
 let poemGeneratorForm = document.querySelector("#poem-generator-form");
-poemGeneratorForm.addEventListener("submit", displayPoem);
+poemGeneratorForm.addEventListener("submit", handleSubmit);
